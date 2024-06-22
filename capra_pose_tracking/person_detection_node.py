c5c847132c6d4450f49a5da305825849990278ab
nathan-gt <nathan.gt.protic@gmail.com>
2024-06-22 09:05:12 -0400

    a bit broken but baby steps

--- capra_pose_tracking/person_detection_node.py
+++ capra_pose_tracking/person_detection_node.py
@@ -1,4 +1,9 @@
+import hashlib
+import os
 import sys
+
+import keras
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from rclpy.publisher import Publisher
@@ -6,13 +11,25 @@ from std_msgs.msg import String
 from geometry_msgs.msg import PointStamped
 
 from .pose_detection.tools.pose_detector import PoseDetector
-import pyzed.sl as sl
 from zed_interfaces.msg import ObjectsStamped
 
 class TrackingNode(Node):
 
     def __init__(self):
         super().__init__('tracking_node')
+        model_path = './pose_detection/trainings/training19/model/model19.keras'
+        checksum_path = './pose_detection/trainings/training19/model/weights_checksum19.txt'
+
+        mlp = keras.saving.load_model(model_path)
+        checksum_after = self.model_weights_checksum(mlp)
+
+        with open(checksum_path, 'r') as f:
+          checksum_before = f.read().strip()
+
+        assert checksum_before == checksum_after, "The weights have not been loaded correctly!"
+        print("The weights have been loaded correctly!")
+        
+        self.poseDetector = PoseDetector(mlp)
         # zed subscription
         self.subscription = self.create_subscription(
             ObjectsStamped,
@@ -24,9 +41,16 @@ class TrackingNode(Node):
         # position/state publisher
         # self.publisherPoint = self.create_publisher(PointStamped, 'tracking/point', 10)
         # self.publisherState = self.create_publisher(PointStamped, 'tracking/state', 10)
+    
+    def model_weights_checksum(model):
+        weights = model.get_weights()
+        weights_concat = np.concatenate([w.flatten() for w in weights])
+        return hashlib.md5(weights_concat).hexdigest()
         
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg)
+    def listener_callback(self, msg:ObjectsStamped):
+        self.get_logger().info("fufuf")
+        self.poseDetector.detect(msg.objects)
+        # self.get_logger().info('I heard: "%s"' % msg.objects)
         # self.publish_data(self.publisherPoint, msg)
 
     def publish_data(self, publisher: Publisher, data):
@@ -39,6 +63,8 @@ def main(args=None):
     trackingNode = TrackingNode()
 
     rclpy.spin(trackingNode)
+    trackingNode.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
